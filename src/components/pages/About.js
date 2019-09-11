37c173eb8443e4dbe2e400fbc8bf10a177a5505c
Mamadou Bah <bahmousto@gmail.com>
2019-09-11 14:44:03 -0400

    First commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a basic TodoList app build with React and Bootstrap.</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
